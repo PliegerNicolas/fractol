0734f05ad1209ced76e803379d70d3af603d3103
Nicolas PLIEGER <plieger05@gmail.com>
2023-02-10 17:24:54 +0100

    Manage error when env -i. mlx_init needs env.

--- srcs/fract-ol.c
+++ srcs/fract-ol.c
@@ -6,19 +6,24 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:41:23 by nicolas           #+#    #+#             */
-/*   Updated: 2023/02/08 15:06:10 by nplieger         ###   ########.fr       */
+/*   Updated: 2023/02/10 17:14:10 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
 
-static void	initialize_mlx(t_mlx *mlx, t_complex julia_arg)
+static t_bool	initialize_mlx(t_mlx *mlx, t_complex julia_arg)
 {
 	mlx->mlx_ptr = mlx_init();
+	if (!mlx->mlx_ptr)
+		return (TRUE);
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, DISPLAY_LENGTH, DISPLAY_WIDTH,
 			TITLE);
+	if (!mlx->win_ptr)
+		return (TRUE);
 	mlx->vars.img = NULL;
 	mlx->vars.cursor_pos.real = julia_arg.real;
 	mlx->vars.cursor_pos.imag = julia_arg.imag;
+	return (FALSE);
 }
 
 int	main(int argc, char **argv)
@@ -29,11 +34,12 @@ int	main(int argc, char **argv)
 
 	if (!verify_args(argc - 1, argv + 1, &target, &julia_arg))
 		return (1);
-	usage_indications();
 	mlx.mlx_ptr = NULL;
 	mlx.win_ptr = NULL;
 	select_fractal(&mlx, target);
-	initialize_mlx(&mlx, julia_arg);
+	if (initialize_mlx(&mlx, julia_arg))
+		return (1);
+	usage_indications();
 	set_hooks(&mlx);
 	fill_display(&mlx);
 	mlx_loop(mlx.mlx_ptr);
